1b8a4c4836d5d0b8d7bd261b9950ccb7c9d7ae44
Fl0rely <virely.florent@gmail.com>
2024-05-21 21:07:13 +0200

    étape 1: ajout variable, affectation, int, ;, opérateur de base

--- traducteur.c
+++ traducteur.c
@@ -7,18 +7,46 @@
 
 void traducteur(lexeme_list* lexemes)
 {
-    lexeme_list* lex = lexemes->next; // Le lexeme actuel (passe le premier).
+    lexeme_list* lex = lexemes; // Le lexeme actuel (passe le premier).
     while (lex->type != LxmEnd)
     {
-        lex = lex->next;
+        
+        lex = lex->next; 
+        //printf("%d,%s,\n",lex->type,lex->content);
+        if (lex -> type ==LxmType){
+            if (strcmp(lex->next->content,"main")==0){
+                lex =lex->next->next->next->next;
+            }
+            else{
+                printf("let ");
+            }
+            
+        }
+        if (lex->type ==LxmVariable){
+            printf("%s ",lex->content);
+        }
+        if (lex->type == LxmAffectation){
+            printf("%s ref ",lex->content);
+        }
+        if (lex->type ==LxmInt){
+            printf("%s",lex->content);
+        }
+        if (lex->type ==LxmPunctuation){
+            if (strcmp(lex->content, ";")==0){
+                printf(";\n");
+            }
+        }
+        if (lex->type ==LxmOperator){
+            printf("%s",lex->content );
+        }
     }
 }
 
 int main()
 {
     // Cree la liste de lexemes du fichier
-    FILE* source_file = fopen("./test.c", "r");
-    // FILE* source_file = fopen("./tests/etape1/etape1.c", "r");
+    //FILE* source_file = fopen("./test.c", "r");
+    FILE* source_file = fopen("./tests/etape1/etape1.c", "r");
     lexeme_list* l = lexeur(source_file);
 
     printf("\n===\n");
