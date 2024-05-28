d235d4f1dc0cc7cee180e712540907c3108b3c28
JuulesDev <jdessors@gmail.com>
2024-05-28 21:25:24 +0200

    Fix de ";;" et ";", l'indentation en début de ligne, des espaces, les " autours des strings

--- traducteur.c
+++ traducteur.c
@@ -5,124 +5,149 @@
 
 #include "./lexer/lexer.h"
 
-void indentation(int indice){ //fonction pour print l'indentation en fonction de while/ condition 
-    for (int i=0; i< indice;i+=1){
-            printf("   ");
-        }
+/*
+    Affiche l'indentation du code.
+
+    Argument:
+        int depth: La profondeur de l'indentation.
+*/
+void indentation(int depth)
+{
+    for (int i = 0; i < depth; i += 1)
+    {
+        printf("    ");
+    }
 }
+
 void traducteur(lexeme_list* lexemes)
 {
     lexeme_list* lex = lexemes; // Le lexeme actuel (passe le premier).
-    int parenthese_fonction=-1; //nombre de parenthèse après une fonction 
-    int* liste_itérateur=malloc(sizeof(int)*20); // liste pour savoir l'ordre des boucles et conditions 0:boucle 1:condition
-    int indice_liste_itérateur=0;
+    int compteur_parenthese = -1; // Nombre de parenthèses après une fonction 
+
+    // Pile indiquant le bloc dans lequel on se trouve : 0 -> while/for, 1 -> if
+    int* pile_bloc = malloc(sizeof(int)*20);
+    int indice_pile_bloc = 0;
+
     while (lex->type != LxmEnd)
-    {
+    {    
+        lex = lex->next;
         
-        lex = lex->next; 
-        
-        //printf("%d,%s\n",lex->type,lex->content);
+        // printf("-> %d,%s\n",lex->type,lex->content);
         if (lex->type == LxmType){ //Traduction des Types
-            if (strcmp(lex->next->content,"main") == 0) { //cas pour int main ou on skip tout
-                lex =lex->next->next->next->next;
+            if (strcmp(lex->next->content, "main") == 0) { //cas pour int main ou on skip tout
+                lex = lex->next->next->next->next;
             }
             else 
-            { 
+            {
+                indentation(indice_pile_bloc);
                 printf("let "); //cas pour création d'une variable
             }
             
         }
-        else if (lex->type == LxmVariable) { //Traduction des variables
-                if (strcmp(lex->next->content,"=") == 0) { //écriture definition variable
-                printf("%s",lex->content);
-                }
-                else{
-                printf("!%s",lex->content); //écriture valeur variable avec !
-                }
+        else if (lex->type == LxmVariable)
+        { //Traduction des variables
+            if (strcmp(lex->next->content, "=") == 0)
+            { //écriture definition variable
+                indentation(indice_pile_bloc);
+                printf("%s", lex->content);
+            } else {
+                printf("!%s", lex->content); //écriture valeur variable avec !
+            }
             
         }
-        else if (lex->type == LxmAffectation) { //Traduction affectation de variable
-            printf("%s ref ",lex->content);
+        else if (lex->type == LxmAffectation)
+        { //Traduction affectation de variable
+            printf(" = ref ");
         }
-        else if (lex->type == LxmInt) { //Traduction variable entier
-            printf("%s",lex->content);
+        else if (lex->type == LxmInt)
+        { //Traduction variable entier
+            printf("%s", lex->content);
         }
-        else if (lex->type == LxmPunctuation){ //Traduction de fin de ligne
-            if (strcmp(lex->content, ";") == 0) {
-                printf(";;\n"); //double point virgule en fin de ligne 
-                indentation(indice_liste_itérateur); //appel de l'indentation après \n
-            }
-            if (strcmp(lex->content,"(") == 0 ) {
-                if (parenthese_fonction>0) { 
-                    parenthese_fonction=parenthese_fonction+1; //augmente le compteur des parenthèse pour les arguments d'une fonction
+        else if (lex->type == LxmPunctuation)
+        { // Traduction de fin de ligne
+            if (strcmp(lex->content, ";") == 0)
+            {
+                if (indice_pile_bloc > 0)
+                {
+                    printf(";\n"); // Dans un bloc
+                } else {
+                    printf(";;\n"); // Hors d'un bloc
+                }
+            } else if (strcmp(lex->content,"(") == 0 )
+            {
+                if (compteur_parenthese>0) { 
+                    compteur_parenthese += 1; //augmente le compteur des parenthèse pour les arguments d'une fonction
                 }
                 printf("(");
-            }
-            if (strcmp(lex->content,")") == 0) {
-                if (parenthese_fonction == 0) {
-                    parenthese_fonction = -1; //cas de la dernière parenthèse que l'on affiche pas à la fin des arguments d'une fonction 
-                }
-                else{
-                    if (parenthese_fonction>0) {
-                    parenthese_fonction=parenthese_fonction-1; //diminue le compteur des parenthèse en argument
+            } else if (strcmp(lex->content,")") == 0)
+            {
+                if (compteur_parenthese == 0)
+                {
+                    compteur_parenthese = -1; //cas de la dernière parenthèse que l'on affiche pas à la fin des arguments d'une fonction 
+                } else {
+                    if (compteur_parenthese>0)
+                    {
+                        compteur_parenthese -= 1; //diminue le compteur des parenthèse en argument
                     }
                     printf(")");
                 }
-            }
-            if (strcmp(lex->content,"{")==0){ 
-                if (liste_itérateur[indice_liste_itérateur]==0){ // si la dernière fonction utilisée est une boucle 
+            } else if (strcmp(lex->content,"{") == 0)
+            { 
+                if (pile_bloc[indice_pile_bloc] == 0) { // si la dernière fonction utilisée est une boucle 
                     printf("do \n");
-                    indice_liste_itérateur+=1; //augmente le nombre d'itérateurs
-                    indentation(indice_liste_itérateur);
-                    }
-                else{ // si la dernière fonction utilisée est un condtion 
-                    printf("begin \n");
-                    indice_liste_itérateur+=1; //augmente le nombre d'itérateurs
-                    indentation(indice_liste_itérateur);
+                    indice_pile_bloc += 1; //augmente le nombre d'itérateurs
+                } else { // si la dernière fonction utilisée est un condtion 
+                    printf(" (\n");
+                    indice_pile_bloc += 1; //augmente le nombre d'itérateurs
+                }
+            } else if (strcmp(lex->content,"}")==0)
+            {
+                if (indice_pile_bloc>0){
+                    if (pile_bloc[indice_pile_bloc-1]==0){ // si la dernière fonction utilisée est une boucle 
+                        printf("done\n");
+                        indice_pile_bloc-=1; //diminue le nombre d'itérateurs
+                    } else { // si la dernière fonction utilisée est un condtion
+                        indice_pile_bloc -= 1; //diminue le nombre d'itérateurs
+                        indentation(indice_pile_bloc); 
+                        printf(")\n");
                     }
                 }
-            if (strcmp(lex->content,"}")==0){
-                if (indice_liste_itérateur>0){
-                    if (liste_itérateur[indice_liste_itérateur-1]==0){ // si la dernière fonction utilisée est une boucle 
-                        printf("done \n");
-                        indice_liste_itérateur-=1; //diminue le nombre d'itérateurs
-                        indentation(indice_liste_itérateur);
-                        }
-                    else{ // si la dernière fonction utilisée est un condtion 
-                        printf("end \n");
-                        indice_liste_itérateur-=1; //diminue le nombre d'itérateurs
-                        indentation(indice_liste_itérateur);
-                        }
-                    }
             }
         }
         else if (lex->type == LxmOperator) { //cas pour les opérateurs 
-            printf("%s",lex->content );
+            printf("%s", lex->content);
         }
         else if(lex->type == LxmComment) { //cas pour les commentaires
+            indentation(indice_pile_bloc);
             printf("(*");
             printf("%s", lex->content);
             printf("*)\n");
-            indentation(indice_liste_itérateur);
         }
         else if(lex->type == LxmString) {
-            printf("%s ",lex->content);
+            printf("\"%s\" ", lex->content);
         }
-        else if(lex->type == LxmKeyWord) {
-            if (strcmp(lex->content,"printf") == 0) { //cas pour printf
+        else if(lex->type == LxmKeyWord)
+        {
+            if (strcmp(lex->content, "printf") == 0)
+            { //cas pour printf
+                indentation(indice_pile_bloc);
                 printf("Printf.printf ");
                 lex=lex->next; //on passe pour passer la parenthèse
-                parenthese_fonction=0; //initialisation du compteur de parenthèse après une fonction à 0
+                compteur_parenthese=0; //initialisation du compteur de parenthèse après une fonction à 0
             }
-            else{
-            printf("%s ",lex->content); //Cas des mots clés pour les boucles
-            if (strcmp(lex->content,"while")==0 ){
-                liste_itérateur[indice_liste_itérateur]=0; //ajout d'une boucle while dans la pile
+            else
+            {
+                indentation(indice_pile_bloc);
+                printf("%s ",lex->content); //Cas des mots clés pour les boucles
+                if (strcmp(lex->content, "while") == 0)
+                {
+                    pile_bloc[indice_pile_bloc] = 0; //ajout d'une boucle while dans la pile
+                }
+                if (strcmp(lex->content, "if") == 0)
+                {
+                    pile_bloc[indice_pile_bloc] = 1; //ajout d'une condition dans la pile 
+                }
             }
-            if (strcmp(lex->content,"if")==0){
-                liste_itérateur[indice_liste_itérateur]=1; //ajout d'une condition dans la pile 
-            }
-        }
         }
     }
 }
@@ -137,6 +162,9 @@ int main(int argc, char* argv[])
         printf("\n===\n");
 
         print_list(l);
+
+        printf("\n= CODE TRADUIT =\n\n");
+
         traducteur(l);
 
         free_list(l);
